1c8ac191c1abc8b6fe2cd3ea137d3118c15b384f
fran090893 <37314355+fran090893@users.noreply.github.com>
2018-04-17 16:38:20 -0600

    vistas de administrador no funcional

--- application/views/layout/Administradores/menu.php
+++ application/views/layout/Administradores/menu.php
@@ -16,8 +16,8 @@
                           <span class="menu-arrow arrow_carrot-right"></span>
                       </a>
             <ul class="sub">
-              <li><a class="" href="form_component.html">Form Elements</a></li>
-              <li><a class="" href="form_validation.html">Form Validation</a></li>
+              <li><a class="" href="form_component.html">Agregar grupo</a></li>
+              <li><a class="" href="form_validation.html">Ver grupos</a></li>
             </ul>
           </li>
           <li class="sub-menu">
